d3119ee2cba378aab382d2c56a5d4bb26de8883d
sohan-chaudhari <sohanchaudhari384240@gmail.com>
2026-04-07 18:20:37 +0530

    Delete Bad_code.py

--- Bad_code.py
+++
@@ -1,6 +0,0 @@
-class library:
-    def issue_book(self,book_name,is_available):
-        if is_available:
-            print(f"Book {book_name} issued successfully ")
-        else:
-            print(f"Book {book_name} is not available ")
